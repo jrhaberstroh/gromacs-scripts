77de1a8572b58311ff8cd9ad72941feecbf69df5
John Haberstroh <jhaberstroh@berkeley.edu>
2013-04-28 11:04:04 -0700

    Added topology, and added a README

--- make_traj.sh
+++ make_traj.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd EQMDRUN/
-grompp -f ../scripts/short_md.mdp -c eqmdrun.gro -t eqmdrun.cpt -p ../smytopol.top -o eqmdrun
+grompp -f ../scripts/eqstep_md.mdp -c eqmdrun.gro -t eqmdrun.cpt -p ../smytopol.top -o eqmdrun
 mdrun -nt 1 -v -deffnm eqmdrun
 cd ../NEQMDRUN/
 grompp -f ../scripts/neq_md.mdp -c ../EQMDRUN/eqmdrun.gro -t ../EQMDRUN/eqmdrun.cpt -p ../smytopol.top -o neqmdrun -maxwarn 1
