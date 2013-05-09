7ac607604c7e451dcbc19812f8a3d7ccdcae5501
John Haberstroh <jhaberstroh@n0006.scs00>
2013-05-09 11:34:41 -0700

    Revised cluster_traj_submit.sh to generate a .pbs file instead of BEING a .pbs file. Good call.

--- cluster/cluster_traj_prepare.sh
+++ cluster/cluster_traj_prepare.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-#PBS -N prep_traj_submit
-#PBS -q debug
-#PBS -l walltime=00:30:00
-#PBS -j oe
-#PBS -V
 
 usage()
 {
 cat << EOF
 usage: $0 options
 
-This script sets up a collection of directories to submit many serial gromacs jobs on a cluster.
+This script generates a .pbs file, which is a job that generates a collection of directories, spaced by repeated calls to the -f argument. The main purpose of this is to generate decorrelated regions within which to harvest trajectories.
+
 This job is inherently serial, though mdrun can be parallel distributed.
 If MPI is wanted, qsub must be called:
 >> qsub -l mppwidth=NUM_CORES
 That argument must be passed with -P here as well.
 NOTE: (*) options are mandatory and have no defaults.
+NOTE: All files must be given as absolute locations because script will call cd before grompp.
 
 OPTIONS:
    -h      Show this message
@@ -25,7 +22,7 @@ OPTIONS:
    -t (*)  Location of .cpt file (from equilibration, usually)
    -p (*)  Location of .top file to use for parameters
    -P      Run with mdrun_mpi; Specifies the number of cores available. default=0 for mdrun without MPI
-   -b      Folder basename, default is CORE
+   -N      Naming scheme, default is CORE
    -v      Verbose (default = false)
 EOF
 }
@@ -38,9 +35,9 @@ GRO=
 CPT=
 TOP=
 PLL=0
-BASE='CORE'
+NAME='CORE'
 VERBOSE=
-while getopts “h:n:f:c:t:p:P:b:v” OPTION
+while getopts “h:n:f:c:t:p:P:N:v” OPTION
 do
      case $OPTION in
          h)
@@ -65,8 +62,8 @@ do
 	 P)
 	     PLL=$OPTARG
 	     ;;
-	 b)
-	     BASE=$OPTARG
+	 N)
+	     NAME=$OPTARG
 	     ;;
          v)
              VERBOSE=1
@@ -84,40 +81,49 @@ then
      exit 1
 fi
 
-GRO=$(pwd)/$GRO
-TOP=$(pwd)/$TOP
-CPT=$(pwd)/$CPT
-MDP=$(pwd)/$MDP
-
 
 echo $CORES
 echo $TIME
 
+echo "#PBS -N $NAME-gmx-tprep" > $NAME-tprep.pbs
+echo "#PBS -q cm_normal" >> $NAME-tprep.pbs
+echo "#PBS -l nodes=4:ppn=4:catamount" >> $NAME-tprep.pbs
+echo "#PBS -j oe" >> $NAME-tprep.pbs
+echo "#PBS -V" >> $NAME-tprep.pbs
+
 #mdrun $CORES times. Make a new directory for each.
-DIGITS=${#CORES}
-PREVIOUS=
-for CTR in $(eval echo {1..$CORES})
-do
-	CTR_DIGITS=${#CTR}
-	printf -v PRE "%$(echo $DIGITS)d" $CTR
-	STR=${PRE// /'0'}
-	mkdir $BASE$STR #Allows ls to order cores numerically by using leading zeros.
-	cd $BASE$STR 
-	mkdir INIT
-	mkdir TRAJ
-	cd INIT
-	if [[ -z $PREVIOUS ]]
-		then grompp -f $MDP -c $GRO -p $TOP -t $CPT -o init$STR.tpr
-	else
-		grompp -f $MDP -c $GRO -p $TOP -t $PREVIOUS -o init$STR.tpr
-	fi
+echo "DIGITS=${#CORES}" >> $NAME-tprep.pbs
+echo "PREVGRO=" >> $NAME-tprep.pbs
+echo "PREVCPT=" >> $NAME-tprep.pbs
+
+echo "module load gromacs" >> $NAME-tprep.pbs
+echo 'cd $PBS_O_WORKDIR' >> $NAME-tprep.pbs
+echo "mkdir $NAME-tprep" >> $NAME-tprep.pbs
+echo "cd $NAME-tprep" >> $NAME-tprep.pbs
+
+echo "for CTR in $(eval echo {1..$CORES})" >> $NAME-tprep.pbs
+echo "do" >> $NAME-tprep.pbs
+#Allows ls to order cores numerically by using leading zeros.
+echo '	printf -v PRE "%$(echo $DIGITS)d" $CTR' >> $NAME-tprep.pbs #Stores into PRE the number $CTR with char* length $DIGITS
+echo '	STR=${PRE// /'0'}' >> $NAME-tprep.pbs  			#Replaces the blanks with 0's 
+echo "	mkdir $NAME"'$STR' >> $NAME-tprep.pbs
+echo "	cd $NAME"'$STR' >> $NAME-tprep.pbs
+echo "	mkdir INIT" >> $NAME-tprep.pbs
+echo "	mkdir TRAJ" >> $NAME-tprep.pbs
+echo "	cd INIT" >> $NAME-tprep.pbs
+echo '	if [[ -z $PREVGRO ]]' >> $NAME-tprep.pbs
+echo "		then grompp -f $MDP -p $TOP -c $GRO -t $CPT -o "'init$STR.tpr' >> $NAME-tprep.pbs
+echo "	else" >> $NAME-tprep.pbs
+echo "		grompp -f $MDP -p $TOP "'-c $PREVGRO -t $PREVCPT -o init$STR.tpr' >> $NAME-tprep.pbs
+echo "	fi" >> $NAME-tprep.pbs
 		
-	touch init$STR.tpr
-	if [ $PLL -eq 0 ]
-		then mdrun -v -deffnm init$STR
-	else
-		aprun -n $PLL mdrun_mpi >& test.log
-	fi
-	PREVIOUS=$(pwd)/init$STR.cpt
-	cd ../..
-done
+if [ $PLL -eq 0 ]; then
+	echo '	mdrun -v -deffnm init$STR' >> $NAME-tprep.pbs
+else	
+	echo "	aprun -n $PLL mdrun_mpi "'-deffnm init$STR>& test.log' >> $NAME-tprep.pbs
+fi
+	
+echo '	PREVCPT=$(pwd)/init$STR.cpt' >> $NAME-tprep.pbs
+echo '	PREVGRO=$(pwd)/init$STR.gro' >> $NAME-tprep.pbs
+echo "	cd ../.." >> $NAME-tprep.pbs
+echo "done" >> $NAME-tprep.pbs
