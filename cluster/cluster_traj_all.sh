bf28a1a26d59d393ccdc433e1d2d5fef03e5c43d
John Haberstroh <jhaberstroh@berkeley.edu>
2013-06-10 17:31:25 -0700

    After working so hard to write something cool, I discover that Hopper also does not accept array jobs. Need to rewrite or something...

--- cluster/cluster_traj_all.sh
+++ cluster/cluster_traj_all.sh
@@ -13,12 +13,12 @@ Requires exactly one of [+]
 OPTIONS:
    -h      Show this message
    -o [*]  Output naming scheme; kept consistent between folders & jobs
-   -T [*]  Location of long .mdp file to run at equilibrium; it is run once between folders.
-   -s [*]  Location of short .mdp file to run at equilibrium; it is run once between trajectories in a given folder.
-   -f [*]  Location of .mdp file to run as a trajectory; alternates running with -t.
-   -c [*]  Location of .gro file to use as initial configuration
-   -p [*]  Location of .top file to use for parameters
-   -t [*]  Location of .cpt file to use for initial checkpoint
+   -T [*]  Location (with path) of long .mdp file to run at equilibrium; it is run once between folders.
+   -s [*]  Location (with path) of short .mdp file to run at equilibrium; it is run once between trajectories in a given folder.
+   -f [*]  Location (with path) of .mdp file to run as a trajectory; alternates running with -t
+   -c [*]  Location (with path) of .gro file to use as initial configuration, 
+   -p [*]  Location (with path) of .top file to use for parameters,
+   -t [*]  Location (with path) of .cpt file to use for initial checkpoint, 
    -N      Number of folders to generate (default=100)
    -n      Number of trajectories to generate in each directory (default=100)
    -1 [+]  Setup PBS for hopper@nersc (24 cores per node, each trajectory must utilize a multiple of 24 cores)
@@ -26,7 +26,7 @@ OPTIONS:
    -R      READY TO SUBMIT; pass this argument to run qsub at all appropriate intervals. Without this flag, only .pbs files will be generated.
    -P      Number of processors to request for any instance of the jobs (cluster default)
    -W      Number of warnings allowed by grompp (default=gromacs-default) (-W 1 strongly recommended when bond-lambda values are not set)
-   -w      Walltime (cluster default)
+   -w      Walltime for prep (trajectories each get 1h on Hopper, default on Hopper = 06:00:00) 
    
 EOF
 }
@@ -117,24 +117,34 @@ if ! [[ -z $WARN ]]; then
 	WARN="-maxwarn $WARN"
 fi
 
+
 if [ $CLUSTER = "HOPPER" ]; then
-	echo "Not yet built for Hopper"
+	echo "Hopper in testing... Hopper also does not accept array jobs!!" 
 		
 	GROMACS_VERSION='gromacs/4.6.1-sp'
 	HOPPERMAGIC="CRAY_ROOTFS=DSL"
 	GROMPP="grompp_sp"
 	MDRUN1="aprun -n $P_THREAD mdrun_mpi_sp"
 	MDRUN2="aprun -n $P_THREAD mdrun_mpi_sp"
+	QUEUE1='regular'
 	QUEUE2='regular'
 	CORE_RESOURCE_1="mppwidth=$P_THREAD"
 	CORE_RESOURCE_2="mppwidth=$P_THREAD"
+
+
+	if ! [[ -z $WALL ]]; then
+		WALL="walltime=$WALL"
+	else
+		WALL="walltime=06:00:00"
+	fi
 
 	#---------------------------------------
 	# Setup the folders
 	#---------------------------------------
 	echo "#PBS -N $NAME-gmx-tprep" > $NAME-tprep.pbs
-	echo "#PBS -q cm_normal" >> $NAME-tprep.pbs
+	echo "#PBS -q $QUEUE1" >> $NAME-tprep.pbs
 	echo "#PBS -l $CORE_RESOURCE_1" >> $NAME-tprep.pbs
+	echo "#PBS -l $WALL" >> $NAME-tprep.pbs
 	echo "#PBS -j oe" >> $NAME-tprep.pbs
 	echo "#PBS -V" >> $NAME-tprep.pbs
 	echo " " >> $NAME-tprep.pbs
@@ -187,7 +197,7 @@ if [ $CLUSTER = "HOPPER" ]; then
 	echo "#PBS -N $NAME-gmx-traj" >> $NAME-traj.pbs
 	echo "#PBS -q $QUEUE2" >> $NAME-traj.pbs
 	echo "#PBS -l $CORE_RESOURCE_2" >> $NAME-traj.pbs
-	#echo "#PBS -l walltime=01:00:00" >> $NAME-traj.pbs
+	echo "#PBS -l walltime=01:00:00" >> $NAME-traj.pbs
 	echo "#PBS -j oe" >> $NAME-traj.pbs
 
 	echo 'cd $PBS_O_WORKDIR' >> $NAME-traj.pbs
@@ -239,10 +249,11 @@ fi
 
 
 if [ $CLUSTER = "CATAMOUNT" ]; then
-	echo "Catamount version of this script is not allowed; Catamount does not permit hold jobs or array jobs."	
+	echo "Catamount version of this script is not allowed!!! Catamount does not permit hold jobs or array jobs. Not actually tested due to this."
 	GROMACS_VERSION=
 	GROMPP=
-	MDRUN=
+	MDRUN1=
+	MDRUN2=
 	CORE_RESOURCE_1=
 	CORE_RESOURCE_2=
 	QUEUE2=
@@ -250,7 +261,8 @@ if [ $CLUSTER = "CATAMOUNT" ]; then
 	if [ $P_THREAD -gt 1 ]; then
        	GROMACS_VERSION='gromacs/4.6-mpi'
        	GROMPP="mpirun -n 1 grompp_mpi"
-       	MDRUN="mpirun -n $P_THREAD mdrun_mpi"
+       	MDRUN1="mpirun -n $P_THREAD mdrun_mpi"
+       	MDRUN2="mpirun -n $P_THREAD mdrun_mpi"
        	QUEUE2='cm_normal'
        	CORE_RESOURCE_1="nodes=$(($P_THREAD/16)):ppn=16:catamount"
        	CORE_RESOURCE_2="nodes=$(($P_THREAD/16)):ppn=16:catamount"
@@ -312,7 +324,7 @@ if [ $CLUSTER = "CATAMOUNT" ]; then
        JOBID=
        if [[ $READY ]]; then
        	echo "qsub $NAME-tprep.pbs"
-       	JOBID=`qsub $NAME-tprep.pbs`
+       	#JOBID=`qsub $NAME-tprep.pbs`
        	echo "tprep: $JOBID"
 	fi
 	
@@ -368,6 +380,6 @@ if [ $CLUSTER = "CATAMOUNT" ]; then
 	
 	if [ $READY ]; then
        		echo "qsub $NAME-traj.pbs -t 1-$NFOLD -W depend=afterok:$JOBID"
-       		qsub $NAME-traj.pbs -t 1-$NFOLD -W depend=afterok:$JOBID
+       		#qsub $NAME-traj.pbs -t 1-$NFOLD -W depend=afterok:$JOBID
 	fi
 fi
